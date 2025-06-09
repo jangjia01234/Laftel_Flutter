032699deebcb960aa0788856f98ba2a279c2895a
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-06-09 11:24:56 +0900

    laftel Flask 래퍼 서버 및 테스트용 Flutter 검색 화면 추가

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'widgets/common/bottom_tab_bar.dart';
 import 'screens/video_player_screen.dart';
 import 'screens/event_screen.dart';
-import 'screens/http_rest_api.dart';
+import 'screens/anime_search_screen.dart';
 
 final routes = {
   // '/': (BuildContext context) => BottomTabBar(),
-  '/': (BuildContext context) => HttpRestApi(),
+  '/': (BuildContext context) => AnimeSearchScreen(),
   '/video': (BuildContext context) => VideoPlayerScreen(),
   '/events': (BuildContext context) => EventScreen(),
 };
---
+++ lib/utils/flask_test.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+import laftel
+
+app = Flask(__name__)
+
+@app.route('/search/<query>')
+def search(query):
+    '''
+    GET /search/<query> (ex. http://127.0.0.1:5000/search/나루토)
+    laftel.sync.searchAnime 로 검색 결과(SearchResult 리스트)를 가져온 뒤,
+    필요한 필드(id, name, image, ...)만 뽑아서 JSON 배열로 반환함
+    '''
+    results = laftel.sync.searchAnime(query)
+
+    simplified = []
+    for r in results:
+        # SearchResult 객체에서 화면에 필요한 속성만 골라서 dict 생성
+        simplified.append({
+            "id":        r.id,
+            "name":      r.name,
+            "url":       r.url,
+            "image":     r.image,
+            "adultonly": r.adultonly,
+            "genres":    r.genres,
+        })
+    return jsonify(simplified)
+
+@app.route('/anime/<int:id>')
+def anime_info(id):
+    info = laftel.sync.getAnimeInfo(id)  # AnimeInfo
+    return jsonify(info.__dict__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
